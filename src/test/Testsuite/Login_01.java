78216e3f433989ace917ff9d5141a2dd0a29bbbd
Sandeep Duve <sandeep.duve@gmail.com>
2018-09-30 23:44:46 +0530

    update comment

--- src/test/Testsuite/Login_01.java
+++ src/test/Testsuite/Login_01.java
@@ -3,6 +3,12 @@ import PageObjectModel.preCondition;
 import Pages.Login;
 import org.testng.annotations.Test;
 
+
+/**
+ * created by : Sandeep Duve
+ * crated at: 11:42 PM: 30-9-2018
+ */
+
 public class Login_01 extends preCondition
 {
     @Test(priority = 1, description = "Login Test on FreeCRM.com")
